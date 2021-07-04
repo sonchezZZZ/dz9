6ff7ad066279d8495f1f271b6694c4a99c376675
Sofiia Cherednychenko <ruslanovna1604@gmail.com>
2021-07-04 22:56:24 +0300

    formatting code

--- src/StringLinkedListImpl.java
+++ src/StringLinkedListImpl.java
@@ -1,19 +1,19 @@
-public class StringLinkedListImpl implements StringLinkedList{
+public class StringLinkedListImpl implements StringLinkedList {
     private Node first;
     private Node last;
     private Node head;
 
 
-    static int size=-1;
+    static int size = -1;
 
     @Override
     public void addFirst(String s) {
-        size ++;
-        if(first == null){
+        size++;
+        if (first == null) {
             first.setPrevious(null);
             first.setNext(head);
             head = first;
-        }else{
+        } else {
             Node stock = first;
             first = new Node();
             first.setNext(stock);
@@ -25,13 +25,13 @@ public class StringLinkedListImpl implements StringLinkedList{
 
     @Override
     public void add(String s, int index) {
-        if (index>size){
+        if (index > size) {
             return;
         }
         size++;
         Node oldNode = first;
-        Node prevNode ;
-        for(int i=0;i<=index;i++){
+        Node prevNode;
+        for (int i = 0; i <= index; i++) {
             oldNode = oldNode.getNext();
         }
         prevNode = oldNode.getPrevious();
@@ -63,15 +63,15 @@ public class StringLinkedListImpl implements StringLinkedList{
     @Override
     public String get(int i) {
         String result = " ";
-        if(i == 0){
+        if (i == 0) {
             result = getFirst();
         }
-        if(i == size){
+        if (i == size) {
             result = getLast();
         }
-        if(i<=size){
+        if (i <= size) {
             Node node = first;
-            for (int j=0;j<=i;j++){
+            for (int j = 0; j <= i; j++) {
                 node = node.getNext();
             }
             result = node.getValue();
@@ -87,7 +87,7 @@ public class StringLinkedListImpl implements StringLinkedList{
     @Override
     public String removeFirst() {
         Node node = first.getNext();
-        if(size!=0){
+        if (size != 0) {
             first = node;
             first.setPrevious(null);
             size--;
@@ -97,9 +97,9 @@ public class StringLinkedListImpl implements StringLinkedList{
 
     @Override
     public void remove(int i) {
-        if(i<=size){
+        if (i <= size) {
             Node node = first;
-            for (int j=0;j<=i;j++){
+            for (int j = 0; j <= i; j++) {
                 node = node.getNext();
             }
             Node prevNode = node.getPrevious();
@@ -113,7 +113,7 @@ public class StringLinkedListImpl implements StringLinkedList{
 
     @Override
     public String removeLast() {
-        if(last.getValue()!=null){
+        if (last.getValue() != null) {
             Node node = last.getPrevious();
             last = node;
             last.setNext(null);
